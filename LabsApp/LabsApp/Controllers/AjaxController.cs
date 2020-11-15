3dc8e75e3eb3d5f0379743885b203ee1ee164eee
Mateusz Hanzel <matek997@outlook.com>
2020-11-15 15:26:06 +0100

    Implement saving data from form

--- LabsApp/LabsApp/Controllers/AjaxController.cs
+++ LabsApp/LabsApp/Controllers/AjaxController.cs
@@ -3,20 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LabsApp.Models;
+using LabsApp.Web.Database;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LabsApp.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class AjaxController : ControllerBase
     {
-        public AjaxResult Post()
+        private readonly LabsAppDbContext _dbContext;
+        public AjaxController(LabsAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public AjaxResult Post(CompanyModel company)
         {
             var res = new AjaxResult();
             res.success = true;
 
+            var entity = new ItemEntity
+            {
+                Name = company.Name,
+                Description = company.Description,
+                IsVisible = company.IsVisible,
+            };
+
+            _dbContext.Items.Add(entity);
+            _dbContext.SaveChanges();
             return res;
         }
 

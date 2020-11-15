eb3c37910ae3ff70e56355d1bfbe4908d5c6e64b
Mateusz Hanzel <matek997@outlook.com>
2020-11-15 15:25:53 +0100

    Add DB to project

---
+++ LabsApp/LabsApp/DB/LabAppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LabsApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LabsApp.Web.Database
+{
+    public class LabsAppDbContext : DbContext
+    {
+        public LabsAppDbContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+        public DbSet<ItemEntity> Items { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // fluent configuration ...
+        }
+    }
+}
